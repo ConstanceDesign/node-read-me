2d566330729d21b484f3c952567d61b6a87f6d22
Constance Mitchell <93297081+SnugglesMcGee@users.noreply.github.com>
2022-02-19 10:33:37 -0500

    license bugs

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,36 +1,39 @@
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-// Function that returns a license badge based on which license is passed in
+// TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-
-## License:
-[![license](https://img.shields.io/badge/license-${data.license}-blue.svg)](https://img.shields.io/badge/<SUBJECT>-<STATUS>-<COLOR>.svg)](https://shields.io/)`;
-
-  // Function that returns the license link
-  // If there is no license, return an empty string
-  function renderLicenseLink(license) {
-    if (license !== "None") {
-      return `[License](#license)`;
-    }
-    return "";
+function renderLicenseBadge(license) {
+  if (license !== "None") {
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
   }
+  return "";
+}
 
-  // Function that returns the license section of README
-  // If there is no license, return an empty string
-  function renderLicenseSection(license) {
-    if (license !== "None") {
-      return `## License
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  if (license !== "None") {
+    return `[License](#license)`;
+  }
+  return "";
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if (license !== "None") {
+    return `## License
     
     This project is licensed under the ${license} license.`;
-    }
-    return "";
   }
-  // Function to generate markdown for README
+  return "";
+}
 
-  function generateMarkdown(data) {
-    return `# ${data.title}
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
   ${renderLicenseBadge(data.license)}
+  
+  ## About The Project
+  ${data.description}
   
 ## Table of Contents
 
@@ -96,7 +99,6 @@ ${data.test}
 * Date: ${data.date}
 * Author: ${data.author}
 * Email: [${data.email}](mailto:user@example.com) `;
-  }
 }
 
 module.exports = generateMarkdown;
